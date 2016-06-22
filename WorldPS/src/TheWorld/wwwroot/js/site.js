29479839dd64bdb7c12f96d72bda7e8811e5e8a8
cormacgall <cormacgall@hotmail.com>
2016-06-22 10:25:30 +0100

    Add files via upload

---
+++ WorldPS/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// site.js
+
+(function () {
+
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Joe";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style["background-color"] = "#888";
+    };
+    main.onmouseleave = function () {
+        main.style = "";
+    };
+}) ();
+
